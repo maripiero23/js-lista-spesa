32d1e4690b2b9503833e1f40fd33922d78e20aea
maripiero23 <mari.pieropan@gmail.com>
2022-11-07 14:55:47 +0100

    lista spesa

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const spesa = ["pane", "verdura", "frutta"];
+
+let i = 0;
+
+while(spesa.length<3){
+    
+    i++;
+
+}
+
+console.log( spesa);
+
+const listaEl =document.querySelector(".listaspesa");
+listaEl.innerHTML = spesa
+
+const firstEl = document.querySelector(".first");
+firstEl.innerHTML = spesa[0];
+
+const secondEl = document.querySelector(".second");
+secondEl.innerHTML = spesa[1];
+
+const thirdEl = document.querySelector(".third");
+thirdEl.innerHTML = spesa[2];
